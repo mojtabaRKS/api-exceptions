5233836be84f92e3f57629fc3ea0235ed18c91e5
Mojtaba Rakhisi <mojtaba.rakhisi@gmail.com>
2021-01-03 18:57:29 +0330

    adding exceptions

---
+++ src/Exceptions/CustomUnauthorizedException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Liateam\ApiException\Exceptions;
+
+use Throwable;
+use Illuminate\Http\Response;
+use Liateam\ApiException\Contracts\ApiException;
+
+class CustomUnauthorizedException extends ApiException
+{
+    /**
+     * CustomUnauthorizedException constructor.
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct($message = "", $code = Response::HTTP_FORBIDDEN, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
